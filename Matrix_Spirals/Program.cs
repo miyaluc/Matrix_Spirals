a53722c5afc41f9ac0391512c401e8ce1143abb6
miyaluc <srlucas3@gmail.com>
2018-03-07 16:26:44 -0800

    Add project files.

---
+++ Matrix_Spirals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Matrix_Spirals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //m - rows, n - columns
+        static void Spiralizer(int m, int n, int[,] structure)
+        {
+            int i, k = 0, l = 0;
+
+            //k - this is the starting row index
+            //m - ending row index
+            //l - starting column index
+            //n - ending column index
+            //i - iterator
+
+            //while starting row index < ending row index AND starting column index < ending column index
+        }
+    }
+}
